5b532b746720a3b4765df2caeff666a17969ae50
Martin Helmich <martin.helmich@hs-osnabrueck.de>
2013-01-31 21:54:59 +0100

    Added header and src files.

---
+++ include/zpu.h
@@ -0,0 +1,8 @@
+#ifndef ZPU_H
+#define ZPU_H
+
+int zpu_load_from_file(char* filename);
+int zpu_stop();
+int zpu_start();
+
+#endif
---
+++ src/zpu.c
@@ -0,0 +1,16 @@
+#include "zpu.h"
+
+int zpu_load_from_file(char* filename)
+{
+	return -1;
+}
+
+int zpu_stop()
+{
+	return -1;
+}
+
+int zpu_start()
+{
+	return -1;
+}
